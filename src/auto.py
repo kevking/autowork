0adf16eab3ff9cb1ed154830dae22cf7b94533b1
kevking <fukai26331>
2019-03-13 15:28:03 +0800

    first release

--- src/auto.py
+++ src/auto.py
@@ -23,7 +23,7 @@ class AUTO:
 
         # 点击提交
         self.driver.find_element_by_css_selector('#loginTable > tbody > tr:nth-child(4) > td:nth-child(2) > a').click()
-        time.sleep(9)
+        time.sleep(2)
 
     def click(self, selector):
         element = self.driver.find_element_by_css_selector(selector)
@@ -33,42 +33,94 @@ class AUTO:
         self.driver.find_element_by_id("kw").send_keys(value)
 
     def bussiness(self):
-        # 储蓄存款
-        self.driver.find_element_by_xpath('//*[@id="hisrpt_con"]/ul/li[2]/p/a').click()
-        self.driver.find_element_by_css_selector('#temps\7c 6 > div > div > div:nth-child(4) > div').click()
-        self.driver.find_element_by_css_selector('body > div:nth-child(9) > div.l-box-select-inner > table > tbody > tr:nth-child(4) > td').click()
-        #查询
-        self.driver.find_element_by_css_selector('#searchbtn').click()
-        time.sleep(3)
-        #下载
-        #TODO 检测是否出现了数据
-        self.driver.find_element_by_css_selector('body > div.btnBar > ul > li.toggleBg.borderRight > ul > li:nth-child(2) > a').click()
-        time.sleep(3)
-        self.driver.find_element_by_css_selector('body > div:nth-child(9) > div.l-box-select-inner > table > tbody > tr:nth-child(5) > td').click()
-        #查询
-        self.driver.find_element_by_css_selector('#searchbtn').click()
-        time.sleep(3)
-        #下载
-        #TODO 检测是否出现了数据
-        self.click('body > div.btnBar > ul > li.toggleBg.borderRight > ul > li:nth-child(2) > a')
-        time.sleep(3)
-        self.send_key(Keys.ESCAPE)
-        self.send_key(Keys.ESCAPE)
-        #经营情况总览表
-        self.click('#hisrpt_con > ul > li:nth-child(3) > p > a')
-        self.click('#searchbtn')
-        time.sleep(3)
-        #下载
-        #TODO 检测是否出现了数据
-        self.click('body > div.btnBar > ul > li.toggleBg.borderRight > ul > li:nth-child(2) > a')
-        time.sleep(3)
-        self.send_key(Keys.ESCAPE)
-        self.send_key(Keys.ESCAPE)
+        #点击储蓄存款日报表
+        self.driver.switch_to.frame('contentFrame')
+        ifra = auto.driver.find_element_by_xpath('/html/body/div/div/div[1]/div[3]/div/iframe')
+        self.driver.switch_to.frame(ifra)
+        self.driver.find_element_by_xpath('/html/body/div/div[2]/div/div[2]/div/ul/li[2]/p/a').click()
+        time.sleep(1)
+        #切换到报表界面
+        self.driver.switch_to.default_content()
+        self.driver.switch_to.frame('reportWin')
+        #选择个人保本理财
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/form/div/ul/li[7]/ul/li[2]/div/div/div[3]/div').click()
+        self.driver.find_element_by_xpath('/html/body/div[9]/div[1]/table/tbody/tr[4]/td').click()
+        #点击查询
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/table/tbody/tr/td[1]/div/div').click()
+        time.sleep(1)
+        #点击下载
+        self.driver.switch_to.frame('report')
+        self.driver.find_element_by_xpath('/html/body/div[1]/ul/li[1]/ul/li[2]/a/span').click()
+
+        #选择个人结构性存款
+        self.driver.switch_to.default_content()
+        self.driver.switch_to.frame('reportWin')
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/form/div/ul/li[7]/ul/li[2]/div/div/div[3]/div').click()
+        self.driver.find_element_by_xpath('/html/body/div[9]/div[1]/table/tbody/tr[5]/td').click()
+        #点击查询
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/table/tbody/tr/td[1]/div').click()
+        time.sleep(2)
+        #点击下载
+        self.driver.switch_to.frame('report')
+        self.driver.find_element_by_xpath('/html/body/div[1]/ul/li[1]/ul/li[2]/a/span').click()
+
+        #点击分支行经营情况总览表
+        self.driver.get("http://rpt.mis.bcs:8080/rpt-web/index")
+        self.driver.switch_to.frame('contentFrame')
+        ifra = auto.driver.find_element_by_xpath('/html/body/div/div/div[1]/div[3]/div/iframe')
+        self.driver.switch_to.frame(ifra)
+        self.driver.find_element_by_xpath('/html/body/div/div[2]/div/div[2]/div/ul/li[3]/p/a').click()
+        time.sleep(1)
+        #切换到报表界面
+        self.driver.switch_to.default_content()
+        self.driver.switch_to.frame('reportWin')
+        #点击查询
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/table/tbody/tr/td[1]/div/div').click()
+        time.sleep(1)
+        #点击下载
+        self.driver.switch_to.frame('report')
+        self.driver.find_element_by_xpath('/html/body/div[1]/ul/li[1]/ul/li[2]/a/span').click()
+
+        #点击存贷款日报表
+        self.driver.get("http://rpt.mis.bcs:8080/rpt-web/index")
+        self.driver.switch_to.frame('contentFrame')
+        ifra = auto.driver.find_element_by_xpath('/html/body/div/div/div[1]/div[3]/div/iframe')
+        self.driver.switch_to.frame(ifra)
+        self.driver.find_element_by_xpath('/html/body/div/div[2]/div/div[2]/div/ul/li[4]/p/a').click()
+        time.sleep(1)
+        #切换到报表界面
+        self.driver.switch_to.default_content()
+        self.driver.switch_to.frame('reportWin')
+        #点击查询
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/table/tbody/tr/td[1]/div/div').click()
+        time.sleep(1)
+        #点击下载
+        self.driver.switch_to.frame('report')
+
+        #点击存款日报表
+        self.driver.get("http://rpt.mis.bcs:8080/rpt-web/index")
+        self.driver.switch_to.frame('contentFrame')
+        ifra = auto.driver.find_element_by_xpath('/html/body/div/div/div[1]/div[4]/div/iframe')
+        self.driver.switch_to.frame(ifra)
+        self.driver.find_element_by_xpath('/html/body/div/div[2]/div/div[2]/div/ul/li[4]/p/a').click()
+        time.sleep(1)
+        #切换到报表界面
+        self.driver.switch_to.default_content()
+        self.driver.switch_to.frame('reportWin')
+        #选择保本理财
+        self.driver.switch_to.default_content()
+        self.driver.switch_to.frame('reportWin')
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/form/div/ul/li[6]/ul/li[2]/div/div/div[3]/div').click()
+        self.driver.find_element_by_xpath('/html/body/div[8]/div[1]/table/tbody/tr[8]/td').click()
+        #点击查询
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/table/tbody/tr/td[1]/div/div').click()
+        time.sleep(1)
+        #点击下载
+        self.driver.switch_to.frame('report')
+
 
 if __name__ == '__main__':
     auto = AUTO()
     auto.login()
-    #auto.bussiness()
-    auto.driver.find_element_by_xpath('//*[@id="hisrpt_con"]/ul/li[2]/p/a').click()
-
+    auto.bussiness()
 
